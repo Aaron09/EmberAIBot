cece0de9d5bf2a4a8c70de68d7f2c3b3a79decc8
Jonah Casebeer <jonah.casebeer@gmail.com>
2016-10-22 15:31:14 -0500

    moving to debug on aarons

--- event_algorithm_v_1.py
+++ event_algorithm_v_1.py
@@ -6,107 +6,107 @@ from datetime import datetime, timedelta
 from api_calls import *
 
 class time_slot(object):
-		def __init__(self,in_start_time,in_end_time):
-			self.start_time = in_start_time
-			self.end_time = in_end_time
+        def __init__(self,in_start_time,in_end_time):
+            self.start_time = in_start_time
+            self.end_time = in_end_time
 
 class event(object):
-		def __init__(self, input_end_time, input_start_time):
-				self.end_time = input_end_time
-				self.start_time = input_start_time
-		#Params: hour and minute ints.
-		#Returns: whether events objects start time is at that time
-		def event_starts(self,hour, minute):
-				if hour == self.start_time.hour and minute == self.start_time.minute:
-						return True
-				return False
-		#Params: hour and minute ints.
-		#Returns: whether events objects end time is at that time.
-		def event_ends(self,hour, minute):
-				if hour == self.end_time.hour and minute == self.end_time.minute:
-						return True
-				return False
+        def __init__(self, input_end_time, input_start_time):
+                self.end_time = input_end_time
+                self.start_time = input_start_time
+        #Params: hour and minute ints.
+        #Returns: whether events objects start time is at that time
+        def event_starts(self,hour, minute):
+                if hour == self.start_time.hour and minute == self.start_time.minute:
+                        return True
+                return False
+        #Params: hour and minute ints.
+        #Returns: whether events objects end time is at that time.
+        def event_ends(self,hour, minute):
+                if hour == self.end_time.hour and minute == self.end_time.minute:
+                        return True
+                return False
 
 
 class calendar(object):
-		def __init__(self):
-				self.my_events = []
-				self.event_counter = 0
+        def __init__(self):
+                self.my_events = []
+                self.event_counter = 0
 
-		#Params: parsed JSON file for individual user (contains all events)
-		#adds all events as objects from provided calendar data set to the calender object
-		def add_events(self, data_set):
-				for time_set in data_set['calendars']['primary']['busy']:
-						self.my_events.append(event(datetime.datetime.strptime(time_set['end'], "%Y-%m-%dT%H:%M:%Sz"), datetime.datetime.strptime(time_set['start'],"%Y-%m-%dT%H:%M:%Sz")))
+        #Params: parsed JSON file for individual user (contains all events)
+        #adds all events as objects from provided calendar data set to the calender object
+        def add_events(self, data_set):
+                for time_set in data_set['calendars']['primary']['busy']:
+                        self.my_events.append(event(datetime.datetime.strptime(time_set['end'], "%Y-%m-%dT%H:%M:%Sz"), datetime.datetime.strptime(time_set['start'],"%Y-%m-%dT%H:%M:%Sz")))
 
 #Params: 2 calendar objects
 #Returns: A list of size 2 lists containing start of that free block and the end of that free block.
 def checkTime(calendar_list):
-		calendar_collection = calendar_list
-		free_zones_JSON = []
-		in_free_time = False
+        calendar_collection = calendar_list
+        free_zones_JSON = []
+        in_free_time = False
 
-		#These are simply dummy times and will be changed once we check sections larger than 24h
-		start = datetime.datetime(2016,9,20)
-		end = start + timedelta(days=1)
+        #These are simply dummy times and will be changed once we check sections larger than 24h
+        start = datetime.datetime(2016,9,20)
+        end = start + timedelta(days=1)
 
-		for dt in rrule.rrule(rrule.MINUTELY, dtstart=start, until=end):
-			for calendar in calendar_collection:
-				for event in calendar.my_events:
-					if(event.event_starts(dt.hour,dt.minute)):
-							calendar.event_counter +=1
-					elif (event.event_ends(dt.hour,dt.minute)):
-							calendar.event_counter -= 1
-				#check if all cals are free
-				allCalendarsFree = checkCountCalendars(calendar_collection)
+        for dt in rrule.rrule(rrule.MINUTELY, dtstart=start, until=end):
+            for calendar in calendar_collection:
+                for event in calendar.my_events:
+                    if(event.event_starts(dt.hour,dt.minute)):
+                            calendar.event_counter +=1
+                    elif (event.event_ends(dt.hour,dt.minute)):
+                            calendar.event_counter -= 1
+                #check if all cals are free
+                allCalendarsFree = checkCountCalendars(calendar_collection)
 
-				if(allCalendarsFree and not in_free_time):
-					free_zones_JSON.append({
-													"type": "start",
-													"hour": dt.hour,
-													"minute": dt.minute,
+                if(allCalendarsFree and not in_free_time):
+                    free_zones_JSON.append({
+                                                    "type": "start",
+                                                    "hour": dt.hour,
+                                                    "minute": dt.minute,
                                                     "day": dt.day,
                                                     "month": dt.month,
                                                     "year": dt.day
-											})
-					in_free_time = True
-				elif(not allCalendarsFree and in_free_time):
-					free_zones_JSON.append({
-													"type": "end",
-													"hour": dt.hour,
-													"minute": dt.minute,
+                                            })
+                    in_free_time = True
+                elif(not allCalendarsFree and in_free_time):
+                    free_zones_JSON.append({
+                                                    "type": "end",
+                                                    "hour": dt.hour,
+                                                    "minute": dt.minute,
                                                     "day": dt.day,
                                                     "month": dt.month,
                                                     "year": dt.day
-											})
-					in_free_time = False
+                                            })
+                    in_free_time = False
 
-		#if the 2 cals are free until the end of that day
-		if(in_free_time):
-			free_zones_JSON.append({
-											"type": "end",
-											"hour": 23,
-											"minute": 59,
+        #if the 2 cals are free until the end of that day
+        if(in_free_time):
+            free_zones_JSON.append({
+                                            "type": "end",
+                                            "hour": 23,
+                                            "minute": 59,
                                             "day":5,
                                             "month":12,
                                             "year":2016
-									})
+                                    })
 
-		#The Json Object we return.
-		return {
-				'day': 1,
-				'free_zones': free_zones_JSON
-		}
+        #The Json Object we return.
+        return {
+                'day': 1,
+                'free_zones': free_zones_JSON
+        }
 
 
 #Params: A list of calendars
 #Returns: If all the calendars are free at the time checked
 def checkCountCalendars(calendar_collection):
-	count = 0
-	for calendarCHECK in calendar_collection:
-		if(calendarCHECK.event_counter == 0):
-			count+=1
-	return count == len(calendar_collection)
+    count = 0
+    for calendarCHECK in calendar_collection:
+        if(calendarCHECK.event_counter == 0):
+            count+=1
+    return count == len(calendar_collection)
 
 
 #takes json input for specific calendar and creates new calendar object with data from json
@@ -117,8 +117,8 @@ def create_calendar(json_data):
 
 #creates json file with all the free times between calendars
 def create_free_times_json(calendar_list):
-	with open('free_times.json', 'w') as outfile:
-			json.dump(checkTime(calendar_list), outfile)
+    with open('free_times.json', 'w') as outfile:
+            json.dump(checkTime(calendar_list), outfile)
 
 # creates email message with all the free times
 def generate_email_content(data_set):
@@ -131,7 +131,7 @@ def generate_email_content(data_set):
             free_zone_ends.append(str(free_zone["hour"]) + ":" + str(free_zone['minute']).zfill(2))
     message = "Hello, \nBoth parties have the following time slots avaiable to meet: \n"
     for i in range(len(free_zone_starts)):
-		message += free_zone_starts[i] + " to " + free_zone_ends[i] + "\n"
+        message += free_zone_starts[i] + " to " + free_zone_ends[i] + "\n"
     return message
 
 def generate_best_time_email(data_set, best_time):
@@ -177,8 +177,8 @@ def return_time(data_set, best_time):
 #opens json file and creates email content
 def return_free_times():
     if(os.path.isfile('free_times.json')):
-		with open('free_times.json') as data_file:
-				free_times_data = json.load(data_file)
+        with open('free_times.json') as data_file:
+                free_times_data = json.load(data_file)
     return generate_email_content(free_times_data) #return to Aaron for email
 
 #main method to execute whole process
@@ -194,26 +194,16 @@ def main(email_list):
     create_free_times_json(calendars)
     print return_free_times()
 
-def determine_ideal_time(times):
-    freq_times = {}
-    for time in times:
-        preferences = time.split(",")
-        for preference in preferenes:
-            if preferece not in freq_times:
-                freq_times[preferece] = 1
-            else
-                freq_times[preferece] += 1
-
+def determine_ideal_time(freq_times):
     if(os.path.isfile('free_times.json')):
-		with open('free_times.json') as data_file:
-                generate_best_time_email(data_file, max(freq_times, key=freq_times.get))
-				return_time(data_file, max(freq_times, key=freq_times.get))
-
+        with open('free_times.json') as data_file:
+            generate_best_time_email(data_file, max(freq_times, key=freq_times.get))
+            return_time(data_file, max(freq_times, key=freq_times.get))
 
 
 if __name__ == '__main__':
-    main(["email1", "email2"])
-    # calendars = []
+    #main(["email1", "email2"])
+    calendars = []
     # json_file1 = get_freebusy_query("email1", datetime.datetime.utcnow().isoformat() + 'Z', (datetime.datetime.utcnow() + datetime.timedelta(days=1)).isoformat() + 'Z')
     # json_file2 = get_freebusy_query("email2", datetime.datetime.utcnow().isoformat() + 'Z', (datetime.datetime.utcnow() + datetime.timedelta(days=1)).isoformat() + 'Z')
     # print(json_file1)
