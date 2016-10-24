f3e314b48d1c90f01abcc65d24f7712c8c20cb1a
cabosanlucas <kaju99@gmail.com>
2016-10-24 16:41:00 -0500

    static flask webpage?

--- webpage.py
+++ webpage.py
@@ -1,13 +1,16 @@
 from flask import Flask
-app = Flask(__name__)
 import EmberBot
 import os
+
+app = Flask(__name__, static_url_path='index.html')
 
 @app.route("/")
 def hello():
 	os.system("EmberBot.py")
-	return app.send_static_file('index.html')
-	return "Hello World!"
+	return render_template('hello.html', name=name)
+	#return app.send_from_directory
+	#return app.send_static_file('index.html')
+	#return "Hello World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5005')
