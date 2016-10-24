832cdf94953e7e59cfc50f52b463295a2c82c0ca
cabosanlucas <kaju99@gmail.com>
2016-10-24 17:25:02 -0500

    aaron your code is fucking with mine

--- webpage.py
+++ webpage.py
@@ -1,16 +1,18 @@
-from flask import Flask, request
-import EmberBot
+from flask import Flask
+#import EmberBot
 import os
 
 app = Flask(__name__)#, static_url_path='')
 
 @app.route("/")
 def root():
-	os.system("EmberBot.py")
+	#os.system("EmberBot.py")
 	#return render_template('index.html', name=name)
 	#return app.send_from_directory
-	#return app.send_static_file('index.html')
-	return "Hello World!"
+	return app.send_static_file('index.html')
+	#return "Hello World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5005')
+
+#app.run(host='0.0.0.0', port='5005')
